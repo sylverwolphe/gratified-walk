499cc00c3d9ccc4635a36d91e352cf33c4e01b0e
sylverwolphe <140569366+sylverwolphe@users.noreply.github.com>
2025-07-30 19:48:06 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,59 +1,119 @@
-class HQScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'HQScene' });
-    }
-
-    preload() {
-        this.load.image('tiles', '/assets/tilemap.png');
-        this.load.tilemapTiledJSON('hq_map', '/assets/hq_floor1.json');
-        this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-    }
-
-    create() {
-        const map = this.make.tilemap({ key: 'hq_map' });
-        const tileset = map.addTilesetImage('tinytown', 'tiles');
-        const groundLayer = map.createLayer('Ground', tileset, 0, 0);
-        const wallsLayer = map.createLayer('Walls', tileset, 0, 0);
-        const objectsLayer = map.createLayer('Objects', tileset, 0, 0);
-
-        this.player = this.physics.add.sprite(100, 100, 'player');
-        this.player.setCollideWorldBounds(true);
-
-        this.cameras.main.startFollow(this.player);
-        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-        const collisionLayer = map.getLayer('Walls')?.tilemapLayer;
-        if (collisionLayer) {
-            collisionLayer.setCollisionByExclusion([-1]); // Collide with all non-empty tiles in Walls
-            this.physics.add.collider(this.player, collisionLayer);
+// Define scenes as functions that return classes (hoisted)
+function createHQScene() {
+    return class extends Phaser.Scene {
+        constructor() {
+            super({ key: 'HQScene' });
         }
 
-        this.transitionZone = new Phaser.Geom.Rectangle(22 * 16, 2 * 16, 16, 16); // Adjusted for door at (22,2)
-        this.physics.world.enableBody(this.transitionZone);
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
-
-    update() {
-        this.player.setVelocity(0);
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-        } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-        }
-        if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-160);
-        } else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(160);
+        preload() {
+            this.load.image('tiles', '/assets/tilemap.png');
+            this.load.tilemapTiledJSON('hq_map', '/assets/hq_floor1.json');
+            this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
         }
 
-        if (Phaser.Geom.Rectangle.ContainsPoint(this.transitionZone, this.player)) {
-            this.scene.start('StairsScene', { playerX: 100, playerY: 100 });
+        create() {
+            const map = this.make.tilemap({ key: 'hq_map' });
+            const tileset = map.addTilesetImage('tinytown', 'tiles');
+            const groundLayer = map.createLayer('Ground', tileset, 0, 0);
+            const wallsLayer = map.createLayer('Walls', tileset, 0, 0);
+            const objectsLayer = map.createLayer('Objects', tileset, 0, 0);
+
+            this.player = this.physics.add.sprite(100, 100, 'player');
+            this.player.setCollideWorldBounds(true);
+
+            this.cameras.main.startFollow(this.player);
+            this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+            const collisionLayer = map.getLayer('Walls')?.tilemapLayer;
+            if (collisionLayer) {
+                collisionLayer.setCollisionByExclusion([-1]); // Collide with all non-empty tiles in Walls
+                this.physics.add.collider(this.player, collisionLayer);
+            }
+
+            this.transitionZone = new Phaser.Geom.Rectangle(22 * 16, 2 * 16, 16, 16); // Adjusted for door at (22,2)
+            this.physics.world.enableBody(this.transitionZone);
+
+            this.cursors = this.input.keyboard.createCursorKeys();
         }
-    }
+
+        update() {
+            this.player.setVelocity(0);
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-160);
+            } else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(160);
+            }
+            if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-160);
+            } else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(160);
+            }
+
+            if (Phaser.Geom.Rectangle.ContainsPoint(this.transitionZone, this.player)) {
+                this.scene.start('StairsScene', { playerX: 100, playerY: 100 });
+            }
+        }
+    };
 }
 
-// Initialize the game after all classes are defined
+function createStairsScene() {
+    return class extends Phaser.Scene {
+        constructor() {
+            super({ key: 'StairsScene' });
+        }
+
+        preload() {
+            this.load.image('tiles', '/assets/tilemap.png');
+            this.load.tilemapTiledJSON('stairs_map', '/assets/stairs.json');
+            this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
+        }
+
+        create(data) {
+            const map = this.make.tilemap({ key: 'stairs_map' });
+            const tileset = map.addTilesetImage('tinytown', 'tiles');
+            const groundLayer = map.createLayer('Ground', tileset, 0, 0);
+            const wallsLayer = map.createLayer('Walls', tileset, 0, 0);
+            const objectsLayer = map.createLayer('Objects', tileset, 0, 0);
+
+            this.player = this.physics.add.sprite(data.playerX || 100, data.playerY || 100, 'player');
+            this.player.setCollideWorldBounds(true);
+
+            this.cameras.main.startFollow(this.player);
+            this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+            const collisionLayer = map.getLayer('Walls')?.tilemapLayer;
+            if (collisionLayer) {
+                collisionLayer.setCollisionByExclusion([-1]);
+                this.physics.add.collider(this.player, collisionLayer);
+            }
+
+            this.transitionZone = new Phaser.Geom.Rectangle(2 * 16, 10 * 16, 16, 16);
+            this.physics.world.enableBody(this.transitionZone);
+
+            this.cursors = this.input.keyboard.createCursorKeys();
+        }
+
+        update() {
+            this.player.setVelocity(0);
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-160);
+            } else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(160);
+            }
+            if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-160);
+            } else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(160);
+            }
+
+            if (Phaser.Geom.Rectangle.ContainsPoint(this.transitionZone, this.player)) {
+                this.scene.start('HQScene', { playerX: 22 * 16, playerY: 2 * 16 });
+            }
+        }
+    };
+}
+
+// Initialize the game - functions are hoisted so this works
 const config = {
     type: Phaser.AUTO,
     width: 480, // 30 tiles * 16px
@@ -65,62 +125,7 @@ const config = {
             debug: false
         }
     },
-    scene: [HQScene, StairsScene]
+    scene: [createHQScene(), createStairsScene()]
 };
 
 const game = new Phaser.Game(config);
-
-class StairsScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'StairsScene' });
-    }
-
-    preload() {
-        this.load.image('tiles', '/assets/tilemap.png');
-        this.load.tilemapTiledJSON('stairs_map', '/assets/stairs.json');
-        this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-    }
-
-    create(data) {
-        const map = this.make.tilemap({ key: 'stairs_map' });
-        const tileset = map.addTilesetImage('tinytown', 'tiles');
-        const groundLayer = map.createLayer('Ground', tileset, 0, 0);
-        const wallsLayer = map.createLayer('Walls', tileset, 0, 0);
-        const objectsLayer = map.createLayer('Objects', tileset, 0, 0);
-
-        this.player = this.physics.add.sprite(data.playerX || 100, data.playerY || 100, 'player');
-        this.player.setCollideWorldBounds(true);
-
-        this.cameras.main.startFollow(this.player);
-        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-        const collisionLayer = map.getLayer('Walls')?.tilemapLayer;
-        if (collisionLayer) {
-            collisionLayer.setCollisionByExclusion([-1]);
-            this.physics.add.collider(this.player, collisionLayer);
-        }
-
-        this.transitionZone = new Phaser.Geom.Rectangle(2 * 16, 10 * 16, 16, 16);
-        this.physics.world.enableBody(this.transitionZone);
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
-
-    update() {
-        this.player.setVelocity(0);
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-        } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-        }
-        if (this.cursors.up.isDown) {
-            this.player.setVelocityY(-160);
-        } else if (this.cursors.down.isDown) {
-            this.player.setVelocityY(160);
-        }
-
-        if (Phaser.Geom.Rectangle.ContainsPoint(this.transitionZone, this.player)) {
-            this.scene.start('HQScene', { playerX: 22 * 16, playerY: 2 * 16 });
-        }
-    }
-}
